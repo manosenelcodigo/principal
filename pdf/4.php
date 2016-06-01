0cb22366ac302f7f7e6c3b11a4c185b66f8341a8
#manosenelcódigo <peligrosamente@gmail.com>
2016-06-01 11:40:22 -0400

    test
    test

---
+++ pdf/4.php
@@ -0,0 +1,46 @@
+﻿<?php
+require_once("mpdf/mpdf.php");
+
+
+
+$html = '<table class="table table-bordered">';
+for($i=0;$i<10;$i++)
+{
+    $html.='
+    <tr>
+        <td>Número</td>
+        <td>'.$i.'</td>
+    </tr>
+';    
+}
+$html.='</table>';
+$html.='<hr />';
+$html.='<div class="panel panel-info">
+          <div class="panel-heading">Panel heading without title</div>
+          <div class="panel-body">
+            Panel content
+          </div>
+        </div>
+';
+
+
+//==============================================================
+//==============================================================
+//==============================================================
+
+$mpdf=new mPDF('c'); 
+$mpdf->SetDisplayMode('fullpage');
+
+// LOAD a stylesheet
+$estilos = file_get_contents('public/bootstrap/bootstrap.css');
+$mpdf->WriteHTML($estilos,1);
+$mpdf->WriteHTML($html);
+$mpdf->Output();
+exit;
+
+//==============================================================
+//==============================================================
+//==============================================================
+
+
+?>
---
+++ pdf/test_1.php
@@ -0,0 +1,19 @@
+﻿<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utyf-8" />
+        <title>Mi página</title>
+        
+    </head>
+    <body>
+       
+       <h1>PDFs</h1>
+       <ul>
+        <li><a href="1.php">test 1</a></li>
+        <li><a href="2.php">test 2</a></li>
+        <li><a href="3.php">test 3 ( dinámico )</a></li>
+        <li><a href="4.php">test 4 ( con estilos css )</a></li>
+       </ul>
+
+    </body>
+</html>
